b8e7c74094f519feb7b99f9fbdc2aa833d4b43a5
John Paul <jcpickel2@gmail.com>
2020-08-04 16:09:22 -0400

    Various Updates
    Added options panel
    Tweaked the star viewing panel
    Fleshed out settings_data
    Implemented star color variation
    Implemented color weighting system, allows certain colors to appear more frequently (yes, I know it's skewed red, I pulled the weights off of a table online)

--- Sandbox2.py
+++ Sandbox2.py
@@ -1,25 +1,38 @@
 import pygame, sys
+from pygame import Surface
 from pygame.locals import *
 from beta_logic import *
 
 pygame.init()
 
-DISPLAYSURF = pygame.display.set_mode((screen_x, screen_y), 0, 32)
+DISPLAYSURF = pygame.display.set_mode((screen_x, screen_y), 0, 32)                      #Main Display
+STARFIELDSURF = Surface((field_x + (2 * field_buffer), field_y + (2 * field_buffer)))   #Starfield
+OPTIONSURF = Surface((screen_x - field_x - 2 * field_buffer, screen_y))                 #Option Bar
 pygame.display.set_caption('StarMap')
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+NIGHTSKY = (7, 11, 15)
+GRAY = (100, 100, 100)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+NEWBLUE = (43, 67, 244)
 
-DISPLAYSURF.fill(BLACK)
-pygame.draw.circle(DISPLAYSURF, (43, 67, 244), (450, 450), 400, 1)
+STARFIELDSURF.fill(BLACK)
+pygame.draw.circle(STARFIELDSURF, NIGHTSKY, (field_x / 2 + field_buffer, field_y / 2 + field_buffer), 400, 0)
+pygame.draw.circle(STARFIELDSURF, GRAY, (field_x / 2 + field_buffer, field_y / 2 + field_buffer), 400, 1)
 
 
 star_data = star_placer()
 for star in star_data:
-    pygame.draw.circle(DISPLAYSURF, star.color, (star.x, star.y), star.size, 0)
+    pygame.draw.circle(STARFIELDSURF, star.color, (star.x, star.y), star.size, 0)
+
+OPTIONSURF.fill(WHITE)
+
+field_point = (screen_x - field_x - 2 * field_buffer, (screen_y - field_y - 2 * field_buffer) / 2)
+DISPLAYSURF.blit(STARFIELDSURF, field_point)
+DISPLAYSURF.blit(OPTIONSURF, (0, 0))
 
 while True:
     for event in pygame.event.get():
--- beta_logic.py
+++ beta_logic.py
@@ -31,15 +31,20 @@ Size - int
 
 import random
 import math
+from settings_data import Star_Colors, Star_Color_Weight
 
-
-screen_x, screen_y = 900, 900
+field_buffer = 50
+field_inner_buffer = 1
+Options_Width = 200
+field_x, field_y = 800, 800
+screen_x, screen_y = field_x + Options_Width + 2 * field_buffer, field_y + 2 * field_buffer
 star_list = []
-number_of_stars = 2000
-star_max_size = 4
+number_of_stars = 3000
+star_max_size = 3
 star_colors = ['white', 'red', 'blue', 'yellow'] #star colors data comes from settings page, the shade of that color 
-star_colors_RGB = [(43, 67, 244), (255, 255, 255), (255, 0, 0), (0, 255, 0), (0, 0, 255)]
-ex = 20
+#star_colors_RGB = [(43, 67, 244), (255, 255, 255), (255, 0, 0), (0, 255, 0), (0, 0, 255)]
+#star_colors_RGB = [(154, 175, 255), (202, 215, 255), (248, 247, 255), (255, 244, 234), (255, 242, 161), (255, 196, 111), (255, 96, 96)]
+ex = 40
 
 class Star:
     def __init__(self, x, y):
@@ -47,9 +52,23 @@ class Star:
         self.y = y
         self.location = (x,y)
         self.size = int(math.floor(random.random() ** ex * star_max_size))
+        if self.size == 1: self.size = 0
         self.amp = random.randint(self.size, star_max_size)
         #self.color = star_colors_RGB[random.randint(0, len(star_colors_RGB) - 1)]
-        self.color = (43, 67, 244)
+        #self.color = (43, 67, 244)
+        total_weight = 0
+        keys = []
+        for entry in Star_Color_Weight:
+            total_weight += Star_Color_Weight[entry]
+            keys.append(entry)
+        result = int(math.floor(random.random() * total_weight))
+        current_weight = Star_Color_Weight[keys[0]]
+        index = 0
+        while result > current_weight:
+            index += 1
+            current_weight += Star_Color_Weight[keys[index]]
+        self.color = Star_Colors[keys[index]]
+
 
     def cluster(self):
         pass #test without first
@@ -64,9 +83,8 @@ def star_placer():
             stars_added += 1
     return star_list
 
-
-r = 390 #raduis
-Xcenter = Ycenter = 450
+r = field_x / 2 - field_inner_buffer #raduis
+Xcenter = Ycenter = field_x / 2 + field_buffer
 
 def is_in_circle(x, y):
     test = (x - Xcenter) ** 2 + (y - Ycenter) ** 2 <= r ** 2
